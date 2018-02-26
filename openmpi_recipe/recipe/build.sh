54bb0aaebc75d3d2a52760b6b74ea3b56d758ee1
Phil Austin <paustin@eos.ubc.ca>
2018-02-25 16:54:17 -0800

    back to conda_prefix

--- openmpi_recipe/recipe/build.sh
+++ openmpi_recipe/recipe/build.sh
@@ -4,6 +4,10 @@
 set -e
 set -x
 
+export FC="$CONDA_PREFIX/bin/x86_64-conda_cos6-linux-gnu-gfortran"
+export CC="$CONDA_PREFIX/bin/x86_64-conda_cos6-linux-gnu-gcc"
+export CXX="$CONDA_PREFIX/bin/x86_64-conda_cos6-linux-gnu-g++"
+
 export LIBRARY_PATH="$PREFIX/lib"
 
 ./configure --prefix=$PREFIX \

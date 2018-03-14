268762299f8c215eed1d708488644206df19f9b8
Phil Austin <paustin@eos.ubc.ca>
2018-03-14 11:50:29 -0700

    change paths

--- sam_entrain/recipe/build.sh
+++ sam_entrain/recipe/build.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+export CC="$CONDA_PREFIX/bin/mpicc"
+export FC="$CONDA_PREFIX/bin/mpif90"
+export CXX="$CONDA_PREFIX/bin/mpicxx"
 
-export CC=mpicc
-export FC=mpif90
-cmake -D NC_DIR=$PREFIX  MPI_DIR=$PREFIX
+mkdir -p build
+cd build
+rm -rf *
+cmake -D NC_DIR="$CONDA_PREFIX" \
+      -D CMAKE_BUILD_TYPE=Release \
+      -D INSTALL_DIR=$PREFIX .. 
+
 
 make VERBOSE=1
 make install
